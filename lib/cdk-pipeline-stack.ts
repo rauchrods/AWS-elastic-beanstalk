a46955fb254d8de936956f9e26ebbacc30898a9f
Rauch Rodrigues <rauch.rodrigues@koseapp.com>
2023-06-01 12:10:28 +0530

    Add Pre-Prod stage

--- lib/cdk-pipeline-stack.ts
+++ lib/cdk-pipeline-stack.ts
@@ -1,35 +1,52 @@
 import { CodePipeline, CodePipelineSource, ShellStep } from 'aws-cdk-lib/pipelines';
 import { Construct } from 'constructs';
-import {  Stack, StackProps } from 'aws-cdk-lib';
+import { Stack, StackProps } from 'aws-cdk-lib';
 import * as cdk from 'aws-cdk-lib';
+import { CdkEBStage } from './eb-stage';
+
 /**
  * The stack that defines the application pipeline
  */
 export class CdkPipelineStack extends Stack {
-  constructor(scope: Construct, id: string, props?: StackProps) {
-    super(scope, id, props);
+    constructor(scope: Construct, id: string, props?: StackProps) {
+        super(scope, id, props);
 
-    const pipeline = new CodePipeline(this, 'Pipeline', {
-      // The pipeline name
-      pipelineName: 'MyServicePipeline',
+        const pipeline = new CodePipeline(this, 'Pipeline', {
+            // The pipeline name
+            pipelineName: 'MyServicePipeline',
 
-       // How it will be built and synthesized
-       synth: new ShellStep('Synth', {
-         // Where the source can be found
-         input: CodePipelineSource.gitHub('rauchrods/AWS-elastic-beanstalk', 'main',{
+            // How it will be built and synthesized
+            synth: new ShellStep('Synth', {
+                // Where the source can be found
+                input: CodePipelineSource.gitHub('rauchrods/AWS-elastic-beanstalk', 'main', {
 
-            authentication: cdk.SecretValue.secretsManager(`github-token-rauch`), }),
-         
-         // Install dependencies, build and run cdk synth
-         installCommands: ['npm i -g npm@latest'],
-         commands: [
-           'npm ci',
-           'npm run build',
-           'npx cdk synth'
-         ],
-       }),
-    });
+                    authentication: cdk.SecretValue.secretsManager(`github-token-rauch`),
+                }),
 
-    // This is where we add the application stages
-  }
+                // Install dependencies, build and run cdk synth
+                installCommands: ['npm i -g npm@latest'],
+                commands: [
+                    'npm ci',
+                    'npm run build',
+                    'npx cdk synth'
+                ],
+            }),
+        });
+
+        // This is where we add the application stages
+
+        // This is where we add the application stages
+
+        // deploy beanstalk app
+        // For environment with all default values:
+        // const deploy = new CdkEBStage(this, 'Pre-Prod');
+
+        // For environment with custom AutoScaling group configuration
+        const deploy = new CdkEBStage(this, 'Pre-Prod', {
+            minSize: "1",
+            maxSize: "2"
+        });
+        const deployStage = pipeline.addStage(deploy);
+
+    }
 }
